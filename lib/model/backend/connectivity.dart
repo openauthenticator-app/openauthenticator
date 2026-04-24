1eac66e4ce5be948a4a25cd029f93d9817eaa986
Skyost <me@skyost.eu>
2026-04-24 18:08:05 +0200

    refactor(linux): Now using `xdg_desktop_portal` instead of `connectivity_plus`.

--- lib/model/backend/connectivity.dart
+++ lib/model/backend/connectivity.dart
@@ -2,28 +2,70 @@ import 'dart:async';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:open_authenticator/utils/platform.dart';
+import 'package:xdg_desktop_portal/xdg_desktop_portal.dart';
 
 /// The connectivity state provider.
 final connectivityStateProvider = AsyncNotifierProvider<ConnectivityStateNotifier, bool>(ConnectivityStateNotifier.new);
 
 /// Allows to manage the connectivity state.
 class ConnectivityStateNotifier extends AsyncNotifier<bool> {
-  /// The [Connectivity] instance.
-  final Connectivity _connectivity = Connectivity();
-
   @override
   Future<bool> build() async {
-    StreamSubscription<List<ConnectivityResult>>? subscription = _connectivity.onConnectivityChanged.listen(_onConnectivityChanged);
-    ref.onDispose(subscription.cancel);
+    if (currentPlatform == .linux) {
+      XdgDesktopPortalClient xdgDesktopPortalClient = XdgDesktopPortalClient();
+      ref.onDispose(() => unawaited(xdgDesktopPortalClient.close()));
+      Completer<bool> initialState = Completer<bool>();
 
-    List<ConnectivityResult> result = await _connectivity.checkConnectivity();
-    return result.areAcceptable;
-  }
+      void onConnectivityChanged(XdgNetworkStatus status) {
+        bool isAvailable = status.available;
+        if (!initialState.isCompleted) {
+          initialState.complete(isAvailable);
+        }
+        if (ref.mounted) {
+          state = AsyncData(isAvailable);
+        }
+      }
 
-  /// Triggered when the connectivity state has changed.
-  void _onConnectivityChanged(List<ConnectivityResult> result) {
-    if (ref.mounted) {
-      state = AsyncData(result.areAcceptable);
+      void onConnectivityError(Object error, StackTrace stackTrace) {
+        if (!initialState.isCompleted) {
+          initialState.completeError(error, stackTrace);
+        }
+        if (ref.mounted) {
+          state = AsyncError(error, stackTrace);
+        }
+      }
+
+      StreamSubscription<XdgNetworkStatus> subscription = xdgDesktopPortalClient.networkMonitor.status.listen(
+        onConnectivityChanged,
+        onError: onConnectivityError,
+      );
+      ref.onDispose(() => unawaited(subscription.cancel()));
+
+      return initialState.future;
+    } else {
+      Connectivity connectivity = Connectivity();
+
+      void onConnectivityChanged(List<ConnectivityResult> result) {
+        if (ref.mounted) {
+          state = AsyncData(result.areAcceptable);
+        }
+      }
+
+      void onConnectivityError(Object error, StackTrace stackTrace) {
+        if (ref.mounted) {
+          state = AsyncError(error, stackTrace);
+        }
+      }
+
+      StreamSubscription<List<ConnectivityResult>>? subscription = connectivity.onConnectivityChanged.listen(
+        onConnectivityChanged,
+        onError: onConnectivityError,
+      );
+      ref.onDispose(() => unawaited(subscription.cancel()));
+
+      List<ConnectivityResult> result = await connectivity.checkConnectivity();
+      return result.areAcceptable;
     }
   }
 }
